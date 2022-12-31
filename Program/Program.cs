5d08bf1450aec843d1c0a85996719faf11ba9470
alex1331 <danilin999@bk.ru>
2022-12-31 16:25:14 +0300

    add Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Print(string[] WriteArray)
+{
+    int size = WriteArray.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{WriteArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] WriteArray = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите [{0}] элемент массива = ", i);
+    WriteArray[i] = Convert.ToString(Console.ReadLine());
+}
+
+string [] MassArray = new string[size];
+int count = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (WriteArray[j].Length <= 3)
+    {
+        MassArray[count] = WriteArray[j];
+        count++;
+    }
+}
+
+Console.WriteLine();
+Print(MassArray);
